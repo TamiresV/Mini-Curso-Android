073f292586753b43f92f54d212c7916403d92fa7
TamiresV <tamiresvilaca@hotmail.com>
2020-07-26 08:35:57 -0300

    Required Fields

--- MiniCursoAndroid/app/src/main/java/com/example/minicursoandroid/Login.java
+++ MiniCursoAndroid/app/src/main/java/com/example/minicursoandroid/Login.java
@@ -3,12 +3,57 @@ package com.example.minicursoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
+
+    public static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    );
+
+    private EditText edtEmail,edtSenha;
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+
+        btLogin = findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!edtEmail.getText().toString().isEmpty() && !edtSenha.getText().toString().isEmpty()){
+
+                }else {
+                    Toast.makeText(Login.this, "E-mail e Senha são obrigatórios", Toast.LENGTH_LONG).show();
+                    if(edtEmail.getText().toString().isEmpty()){
+                        edtEmail.setError("Email obrigatório");
+                    }
+                    if(edtSenha.getText().toString().isEmpty()) {
+                        edtSenha.setError("Senha obrigatória");
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean checkEmail(String email) {
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches();
     }
 }
