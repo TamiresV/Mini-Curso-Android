67b63ad3ee21760665bb7b2071383459c37e7cd9
TamiresV <tamiresvilaca@hotmail.com>
2020-07-27 18:15:51 -0300

    Data Validation

--- MiniCursoAndroid/app/src/main/java/com/example/minicursoandroid/Login.java
+++ MiniCursoAndroid/app/src/main/java/com/example/minicursoandroid/Login.java
@@ -12,6 +12,9 @@ import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
 
+    //EMAIL E SENHA CRIADOS APENAS PARA TESTE
+    private final String emailSalvo = "tamires@teste.com", senhaSalva = "teste";
+
     public static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
             "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                     "\\@" +
@@ -39,8 +42,26 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!edtEmail.getText().toString().isEmpty() && !edtSenha.getText().toString().isEmpty()){
-
+                    //VERIFICA SE EMAIL É VALIDO
+                    if(checkEmail(edtEmail.getText().toString())){
+                        //VERIFICA SE SENHA TEM TAMANHO MAIOR QUE TRÊS
+                        if(edtSenha.getText().toString().length() > 3){
+                            //VERIFICA SE EMAIL E SENHA SÃO AS MESMAS SALVAS NO SISTEMA
+                            if(checkLogin(edtEmail.getText().toString(), edtSenha.getText().toString())){
+                                Toast.makeText(Login.this, "Login efetuado", Toast.LENGTH_LONG).show();
+                            }else{
+                                Toast.makeText(Login.this, "Email ou senha incorretos", Toast.LENGTH_LONG).show();
+                            }
+                        }else{
+                            Toast.makeText(Login.this, "Senha precisa ter 4 ou mais caracteres", Toast.LENGTH_LONG).show();
+                            edtSenha.setError("Senha inválida");
+                        }
+                    }else{
+                        Toast.makeText(Login.this, "E-mail inválido", Toast.LENGTH_LONG).show();
+                        edtEmail.setError("Email inválido");
+                    }
                 }else {
+                    //SE CAMPOS SENHA E/OU EMAIL ESTIVEREM VAZIOS DA ALERTA PARA USUÁRIO
                     Toast.makeText(Login.this, "E-mail e Senha são obrigatórios", Toast.LENGTH_LONG).show();
                     if(edtEmail.getText().toString().isEmpty()){
                         edtEmail.setError("Email obrigatório");
@@ -55,5 +76,15 @@ public class Login extends AppCompatActivity {
 
     private boolean checkEmail(String email) {
         return EMAIL_ADDRESS_PATTERN.matcher(email).matches();
+    }
+
+    private boolean checkLogin(String email, String senha){
+        boolean podeEntrar = false;
+
+        if(email.equals(emailSalvo) && senha.equals(senhaSalva)){
+            podeEntrar =true;
+        }
+
+        return podeEntrar;
     }
 }
